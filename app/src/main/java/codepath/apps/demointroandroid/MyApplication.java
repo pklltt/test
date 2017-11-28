d4934ff48e52193d8ffbd327ff4568983a54bea0
pklltt <34024102+pklltt@users.noreply.github.com>
2017-11-28 16:48:01 +0700

    Update MyApplication.java

--- app/src/main/java/codepath/apps/demointroandroid/MyApplication.java
+++ app/src/main/java/codepath/apps/demointroandroid/MyApplication.java
@@ -1,11 +1,11 @@
 package codepath.apps.demointroandroid;
-
+import com.bugsnag.android.Bugsnag;
 import android.app.Application;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Bugsee.launch(this, "8c7e2f6a-c771-48ae-820e-9fbcf6f5f8cf");
+        Bugsnag.init(this);
     }
 }
