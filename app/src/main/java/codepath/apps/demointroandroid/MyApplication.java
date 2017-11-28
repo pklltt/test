41b10fa2d56e4af0fbdb0a1f55e8c131a5cf7468
pklltt <34024102+pklltt@users.noreply.github.com>
2017-11-28 11:19:20 +0700

    Update MyApplication.java

--- app/src/main/java/codepath/apps/demointroandroid/MyApplication.java
+++ app/src/main/java/codepath/apps/demointroandroid/MyApplication.java
@@ -1,9 +1,11 @@
+package codepath.apps.demointroandroid;
+
 import android.app.Application;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//         Bugsee.launch(this, "8c7e2f6a-c771-48ae-820e-9fbcf6f5f8cf");
+        Bugsee.launch(this, "8c7e2f6a-c771-48ae-820e-9fbcf6f5f8cf");
     }
 }
